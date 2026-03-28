2f678d276432eac9d624389943cf25a6c355085c
sariyachanh-dotcom <sariya.chanh@bumail.net>
2026-03-28 15:18:49 +0700

    Current Script Gun

---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 20f;
+    public float range = 100f;
+    public Camera fpscam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
