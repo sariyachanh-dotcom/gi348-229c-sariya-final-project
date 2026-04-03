4190c781399da08493eaafd2145a72c4f817cc25
sariyachanh-dotcom <sariya.chanh@bumail.net>
2026-04-03 19:57:57 +0700

    Edit PlayerMovement Script

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -3,13 +3,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpForce = 5f;
+
+    public Rigidbody rb;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
 
     void Update()
     {
+        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-        transform.position += move * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + move * speed * Time.deltaTime);
+
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+        }
     }
 }
